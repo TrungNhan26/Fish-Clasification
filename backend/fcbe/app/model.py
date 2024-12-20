63e499d5af04391ca019d565032b72b22808a212
Trung Nhân <126488892+TrungNhan26@users.noreply.github.com>
2024-12-20 08:28:16 +0700

    Delete backend/fcbe directory

--- backend/fcbe/app/model.py
+++
@@ -1,9 +0,0 @@
-from tensorflow.keras.models import load_model
-
-MODEL_PATH = "saved_model/fish_classification.h5"
-
-
-def load_fish_model():
-    # Load the model as usual
-    model = load_model(MODEL_PATH, compile=False)
-    return model
